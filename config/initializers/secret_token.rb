4f8b90ddd3bf8667ebcec1f0b9459843040941ea
Juan Pablo Marzetti <yonpols@gmail.com>
2011-08-17 11:24:25 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sequrecrm::Application.config.secret_token = '2181efc8a6d98c40a03dc38991411ff2db9bed0ad81171a8d3528a33908e6ae2109074684b1c5203a3e74fd91e8ecaf804d3134281c27b73fa2cf03500bcc0f2'
