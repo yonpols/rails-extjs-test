2f6188a509f4b4bde42f16771fdd9a040ecb6aac
Juan Pablo Marzetti <yonpols@gmail.com>
2011-08-18 11:41:08 -0300

    Basic client and contact editing

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,4 @@
 class Client < ActiveRecord::Base
   has_many :contacts
-  accepts_nested_attributes_for :contacts, :reject_if => lambda { |a| a[:content].blank? }, :allow_destroy => true
+  accepts_nested_attributes_for :contacts, :reject_if => lambda { |a| a[:name].blank? }, :allow_destroy => true
 end
